5ab62c077521e0c47072e044709734219dfe9ee7
harsh <focusedstudytarget2022@gmail.com>
2024-03-13 22:34:27 +0530

    Add Express.js, MongoDB, and Socket.io; use Nodemon for development.

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,31 @@
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from './routes/user.routes.js';
+
+import connectToMongoDB from './db/connectToMongoDB.js'
+import cookieParser from "cookie-parser";
+
 const app = express();
-app.listen(6000, ()=>{
-    console.log("Server listening at port 6000");
-})
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+
+app.use(express.json()); // Middleware for parsing JSON bodies
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+// app.get("/", (req, res)=>{
+//     //root route http://localhost:5000/
+//     res.send("Hello World!");
+// });
+
+app.listen(PORT, ()=>{ 
+    connectToMongoDB();
+    console.log(`Server listening at port ${PORT}`)
+});
